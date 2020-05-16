ef0d257e1a08b00629e24dab191d8af97aa71947
Albert <nestle49@yandex.ru>
2020-05-16 12:54:01 +0200

    update: plugins

--- wp-content/themes/alexorto/page-catalog.php
+++ wp-content/themes/alexorto/page-catalog.php
@@ -12,6 +12,14 @@ get_header();
 ?>
 
 <main class="catalog-page">
+            <?php
+				wp_nav_menu( array(
+					'theme_location'  => 'menu-5',
+					'menu'            => 'catalog-menu',
+					'container'       => 'nav',
+					'container_class' => 'page-menu',
+				) );
+			?>
 </main>
 
 <?php
